baf09664817aeaab1d86c5ab3bea81b2e07d30ee
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-30 11:02:21 -0600

    Removed redundant relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 # User
 class User < ApplicationRecord
   validates_presence_of :name
-
-  has_many :reviews
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,4 @@ describe User, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
-
-  describe 'relationships' do
-    it { is_expected.to have_many(:reviews) }
-  end
 end
